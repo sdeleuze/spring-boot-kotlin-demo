07becf59e8d66d71b9476966a1923f226f38a373
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-02-01 09:23:31 +0100

    No need of method attribute in the @RequestMapping annotation for now

--- src/main/kotlin/hello/CustomerController.kt
+++ src/main/kotlin/hello/CustomerController.kt
@@ -2,13 +2,12 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CustomerController @Autowired constructor(val repository:CustomerRepository) {
 
-    @RequestMapping("/", method = arrayOf(RequestMethod.GET))
+    @RequestMapping("/")
     fun list(): Iterable<Customer> {
         return repository.findAll()
     }
