df260dcc2c4929c51318663126ba138bc6696dba
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-02-05 09:25:28 +0100

    Add CustomerController#findByLastName()

--- src/main/kotlin/hello/CustomerController.kt
+++ src/main/kotlin/hello/CustomerController.kt
@@ -1,6 +1,7 @@
 package hello
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController @Autowired constructor(val repository:CustomerRepository) {
 
 	@RequestMapping("/")
-	fun list(): Iterable<Customer> {
+	fun findAll(): Iterable<Customer> {
 		return repository.findAll()
+	}
+
+	@RequestMapping("/{lastName}")
+	fun findByLastName(@PathVariable lastName:String): Iterable<Customer> {
+		return repository.findByLastName(lastName)
 	}
 
 }
--- src/main/kotlin/hello/CustomerRepository.kt
+++ src/main/kotlin/hello/CustomerRepository.kt
@@ -1,9 +1,8 @@
 package hello
 
-import java.util.List
 import org.springframework.data.repository.CrudRepository
 
 interface CustomerRepository : CrudRepository<Customer, Long> {
 
-	fun findByLastName(lastName: String): List<Customer>
+	fun findByLastName(lastName: String): Iterable<Customer>
 }
