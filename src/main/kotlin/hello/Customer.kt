f63e89c0be12bf210763359148d56ad93f60cd6b
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-02-15 19:17:25 +0100

    Polishing code style

--- src/main/kotlin/hello/Customer.kt
+++ src/main/kotlin/hello/Customer.kt
@@ -6,7 +6,9 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-data class Customer(var firstName: String = "",
-					var lastName: String = "",
-					@Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long = 0)
-
+data class Customer(
+		var firstName: String = "",
+		var lastName: String = "",
+		@Id @GeneratedValue(strategy = GenerationType.AUTO)
+		var id: Long = 0
+)
