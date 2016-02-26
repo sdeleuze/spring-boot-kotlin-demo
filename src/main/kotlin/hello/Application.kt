902c719352c7e25ba07fd9cb29d5025944c00c7f
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-02-26 15:13:17 +0100

    Update Jackson Kotlin module and register it in ObjectMapper

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -1,15 +1,21 @@
 package hello
 
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @SpringBootApplication
 open class Application {
 
 	private val log = LoggerFactory.getLogger(Application::class.java)
+
+	@Bean
+	open fun objectMapperBuilder(): Jackson2ObjectMapperBuilder
+		= Jackson2ObjectMapperBuilder().modulesToInstall(KotlinModule())
 
 	@Bean
 	open fun init(repository: CustomerRepository) = CommandLineRunner {
