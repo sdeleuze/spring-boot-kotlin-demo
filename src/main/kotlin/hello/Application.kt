aa1ab36afbb3926a499e8fae8d2347fbc5e8a8f9
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-11-26 15:51:27 +0100

    Polishing

--- src/main/kotlin/hello/Application.kt
+++ src/main/kotlin/hello/Application.kt
@@ -1,12 +1,10 @@
 package hello
 
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @SpringBootApplication
 open class Application {
--- src/test/kotlin/hello/ApplicationTests.kt
+++ src/test/kotlin/hello/ApplicationTests.kt
@@ -2,13 +2,11 @@ package hello
 
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringJUnit4ClassRunner::class)
-@SpringApplicationConfiguration(classes = arrayOf(Application::class))
-@WebAppConfiguration
+@RunWith(SpringRunner::class)
+@SpringBootTest
 class ApplicationTests {
 
 	@Test
