7ae4b7bc25ee4e58bad1ceb80d770dfd34171bb1
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-11-26 15:44:42 +0100

    Switch to a Kotlin based Gradle build file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring Boot Kotlin sample project
+
+This is the source code for the a sample Spring Boot application developed with Kotlin and Spring Data JPA. 
+
+You can launch the application with by running:
+
+		$ ./gradlew bootRun
+
+This project uses a [Kotlin based Gradle](https://blog.gradle.org/kotlin-meets-gradle) configuration.
+Make sure you have at least IntelliJ IDEA 2016.2.5 and Kotlin plugin 1.1-M02 to have
+`build.gradle.kts` auto-complete and validation working correctly. You may have to
+configure the EAP update site in:
+Tools -> Kotlin -> Configure Kotlin Plugin Updates -> Early Access Preview 1.1
+
+This project is Apache 2.0 licensed.
---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+buildscript {
+	val springBootVersion = "1.4.2.RELEASE"
+	val kotlinVersion = "1.0.5-2"
+	extra["kotlinVersion"] = kotlinVersion
+
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		classpath("org.springframework.boot:spring-boot-gradle-plugin:${springBootVersion}")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+	}
+}
+
+apply {
+	plugin("kotlin")
+	plugin("org.springframework.boot")
+}
+
+version = "0.0.1-SNAPSHOT"
+
+configure<JavaPluginConvention> {
+	setSourceCompatibility(1.8)
+	setTargetCompatibility(1.8)
+}
+
+repositories {
+	mavenCentral()
+}
+
+val kotlinVersion = extra["kotlinVersion"] as String
+
+
+dependencies {
+	compile("org.springframework.boot:spring-boot-starter-web")
+	compile("org.springframework.boot:spring-boot-starter-data-jpa")
+	compile("com.h2database:h2")
+	compile("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+	compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.4")
+	testCompile("org.springframework.boot:spring-boot-starter-test")
+}
+
