f490f0dbd2c7540b2700939b3b14eef12548a012
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-10-13 10:41:06 +0200

    Tune Gradle build and switch to JUnit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-	repositories {
-		mavenCentral()
-		maven("https://repo.spring.io/milestone")
-	}
-
-	dependencies {
-		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.M5")
-	}
-}
-
 plugins {
 	val kotlinVersion = "1.1.51"
 	id("org.jetbrains.kotlin.jvm") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 	id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+	id("org.springframework.boot") version "2.0.0.M5"
+	id("org.junit.platform.gradle.plugin") version "1.0.1"
 	id("io.spring.dependency-management") version "1.0.3.RELEASE"
-}
-
-apply {
-	plugin("org.springframework.boot")
 }
 
 version = "1.0.0-SNAPSHOT"
@@ -45,6 +32,10 @@ dependencies {
 	compile("com.h2database:h2")
 	compile("org.jetbrains.kotlin:kotlin-stdlib")
 	compile("org.jetbrains.kotlin:kotlin-reflect")
-	testCompile("org.springframework.boot:spring-boot-starter-test")
+	testCompile("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "junit")
+	}
+	testCompile("org.junit.jupiter:junit-jupiter-api")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine")
 }
 
--- src/test/kotlin/hello/ApplicationTests.kt
+++ src/test/kotlin/hello/ApplicationTests.kt
@@ -1,14 +1,14 @@
 package hello
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
