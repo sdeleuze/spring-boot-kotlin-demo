0af40a349637942bef7a369ae20edf3c1a5c1aa4
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-10-13 10:41:51 +0200

    Use data class for entity

--- src/main/kotlin/hello/Customer.kt
+++ src/main/kotlin/hello/Customer.kt
@@ -6,13 +6,8 @@ import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Customer(
+data class Customer(
 		val firstName: String,
 		val lastName: String,
 		@Id @GeneratedValue(strategy = GenerationType.AUTO)
-		val id: Long = -1) {
-
-	override fun toString(): String {
-		return "Customer(id=$id, firstName='$firstName', lastName='$lastName')"
-	}
-}
+		val id: Long = -1)
